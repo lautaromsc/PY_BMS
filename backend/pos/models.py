93c65d1628a74f26b43247a5448539aa2f10e962
lautaromsc <lautaromsc@gmail.com>
2020-03-31 18:45:34 -0400

    added of model employee

--- backend/pos/models.py
+++ backend/pos/models.py
@@ -8,3 +8,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=100),
+    emp_code = models.CharField(max_length=3),
+    mobile = models.CharField(max_length=15)

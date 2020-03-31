8b64dfb2354d3ddca51c2a0bf3207358bbc6ad3f
lautaromsc <lautaromsc@gmail.com>
2020-03-31 18:37:16 -0400

    starting with postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'pos',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bms',
+        'USER': 'lautaro',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
